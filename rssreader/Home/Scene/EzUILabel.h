b00af7909ea1b03fd1e2741f268a21ef593ed4ca
zhuchao <zhuchao@jasons-MacBook-Pro.local>
2014-12-22 09:55:39 +0800

    push

---
+++ rssreader/Home/Scene/EzUILabel.h
@@ -0,0 +1,13 @@
+//
+//  EzUILabel.h
+//  rssreader
+//
+//  Created by zhuchao on 14/12/22.
+//  Copyright (c) 2014年 zhuchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EzUILabel : UILabel
+
+@end
---
+++ rssreader/Home/Scene/EzUILabel.m
@@ -0,0 +1,21 @@
+//
+//  EzUILabel.m
+//  rssreader
+//
+//  Created by zhuchao on 14/12/22.
+//  Copyright (c) 2014年 zhuchao. All rights reserved.
+//
+
+#import "EzUILabel.h"
+
+@implementation EzUILabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
