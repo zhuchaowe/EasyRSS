9b6327c02a143f922bd1e28ed90d9b696e37ccd5
scott <cgyqqcgy@gmail.com>
2015-09-19 00:33:34 +0800

    Update RssCell.m
    fixed the error: TTTAttributedLabel has no method `init`

--- rssreader/Home/Scene/Main/RssCell.m
+++ rssreader/Home/Scene/Main/RssCell.m
@@ -55,7 +55,7 @@
     _time.textColor = [UIColor whiteColor];
     [self.contentView addSubview:_time];
     
-    _rssTitle = [[TTTAttributedLabel alloc]init];
+    _rssTitle = [[TTTAttributedLabel alloc]initWithFrame:CGRectZero];
     _rssTitle.numberOfLines = 0;
     _rssTitle.lineBreakMode = NSLineBreakByWordWrapping|NSLineBreakByTruncatingTail;
     _rssTitle.textAlignment = NSTextAlignmentLeft;
